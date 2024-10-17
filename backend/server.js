6b73ef9af5c769dcae695d35e190460d3c34adb9
21002624 <akasharul2407@gmail.com>
2024-10-17 15:13:22 +0530

    final touch

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
@@ -19,6 +21,7 @@ cloudinary.config({
 const app=express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
 app.use(express.json({limit : "5mb"}));
 app.use(express.urlencoded({extended: true}));
 
@@ -32,8 +35,16 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes); // middleware to pareser to body
 app.use("/api/users",userRoutes);
-app.use("/api/posts", postRoutes)
-app.use("/api/notifications", notificationRoute)
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoute);
+
+if(process.env.NODE_ENV ===  "production" ){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend", "dist" , "index.html"));
+    })
+} 
 
 
 app.listen(PORT,()=>{
